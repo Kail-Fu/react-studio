c848ac2d673b451c414dd882cc742d08bb07d9ab
Kail Fu <Kailiang_fu@brown.edu>
2022-11-10 21:42:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import bakeryData from "./assets/bakery-data.json";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
@@ -8,21 +8,62 @@ bakeryData.forEach((item) => {
 });
 /* ############################################################## */
 
+const Items = ({ todos }) => {
+  return (
+    <>
+      {todos.map((todo, index) => {
+        return <p key={index}>{todo}</p>;
+      })}
+    </>
+  );
+};
+
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [todos, setItems] = useState([]);
+  const [price, setPrice] = useState(0)
+  const addItem = useCallback((name) => {
+    setItems((t) => [...t, name]);
+  });
+
+  function MyButton(props) {
+    return (
+      <button onClick={() => onClickHandler(props.name, props.amount)}>
+        Add one to cart
+      </button>
+    );
+  }
+
+  function onClickHandler(name, amount) {
+    addItem(name)
+    setPrice(price + amount)
+  }
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
+
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        // <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <div className="BakeryItem">
+          <div className="image-box">
+            <img src={item.image} alt={item.description} height={200} />
+          </div>
+          <div className="title">{item.name}</div>
+          <h3 className="subtitle">{item.description}</h3>
+          <div className="prices">{item.price}</div>
+          <MyButton name={item.name} amount={item.price} />
+        </div>
       ))}
 
       <div>
         <h2>Cart</h2>
         {/* TODO: render a list of items in the cart */}
+        <div><Items todos={todos} /></div>
+        <p>Total Price: ${price}</p>
+        <button>Checkout</button>
       </div>
     </div>
   );
